4dee1979e7c7f089755f0ebebeee9fe312ff2d04
Imran Asghar <imranasghar@dyn172-30-145-225.wireless.uwo.pri>
2025-11-19 20:33:58 -0500

    remove db

--- registry/init.sql
+++ registry/init.sql
@@ -8,3 +8,9 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) NOT NULL PRIMARY KEY, 
     password_hash VARCHAR(255) NOT NULL,
     write_access VARCHAR(10) DEFAULT 'NONE');
+INSERT INTO users (username, password_hash, write_access)
+VALUES (
+    'admin',
+    '$2b$12$YWtcqFBtYJZp6w8IrhInZeaV5n399APutEKhbW/PNeo5DADlsIA0e',
+    'FULL'
+);
